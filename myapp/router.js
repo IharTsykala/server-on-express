425ad5754d188e69622d6bc5b08070d8ef229276
Ihar Tsykala <you@example.comSudja09@bk.ru>
2020-01-15 10:55:34 +0300

    ref: refactor code into router

--- myapp/router.js
+++ myapp/router.js
@@ -5,10 +5,10 @@ const UserController = require('./controller')
 const user_controller = new UserController()
 
 const router = new express.Router()
-router.delete('/delete/:id', user_controller.deleteUser)
+router.get('/all',  user_controller.getAllUser)
 router.get('/:id',  user_controller.getUser)
-router.get('/',  user_controller.getAllUser)
 router.post('/add', user_controller.addUser)
 router.put('/update/:id', user_controller.updateUser)
+router.delete('/delete/:id', user_controller.deleteUser)
 
 module.exports = router
