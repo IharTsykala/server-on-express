044a67d7936c33601ec923652abdea424dad5f1f
Ihar Tsykala <Sudja09@bk.ru>
2020-01-29 21:14:32 +0300

    refactor code

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const routerUsers = require("./users/router-users")
 const routerPets = require("./pets/router-pets")
 const routerUpload = require("./upload/router-upload")
 const mongoose = require("mongoose")
-require('dotenv').config()
+require("dotenv").config()
 
 mongoose.connect(process.env.BD, {
   useNewUrlParser: true,
@@ -17,16 +17,18 @@ const port = process.env.PORT || 8080
 app.use(express.json())
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  )
+  next()
+})
 
 app.use("/users", routerUsers)
 app.use("/pets", routerPets)
 app.use(express.static(__dirname + "/public"), routerUpload)
-
 
 app.listen(port, () => {
   console.log("server on port " + port)
