fdfbcfeae9673636a9a27f437a8f7e9c11c22915
Ihar Tsykala <you@example.comSudja09@bk.ru>
2020-01-29 18:04:15 +0300

    feat: add CORS setting

--- app.js
+++ app.js
@@ -15,9 +15,18 @@ mongoose.connect(process.env.BD, {
 const app = express()
 const port = process.env.PORT || 8080
 app.use(express.json())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use("/users", routerUsers)
 app.use("/pets", routerPets)
 app.use(express.static(__dirname + "/public"), routerUpload)
+
 
 app.listen(port, () => {
   console.log("server on port " + port)
