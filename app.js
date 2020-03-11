54241cc957a3ae5b40ad4168d127dd8435a7f47a
Ihar Tsykala <you@example.comSudja09@bk.ru>
2020-03-11 21:00:14 +0300

    fix: fix sockets

--- app.js
+++ app.js
@@ -22,9 +22,10 @@ mongoose.connect(process.env.BD, {
 })
 
 var app = require("express")()
-var server = require("http").Server(app)
+var server = require("http",).Server(app,{reconnection: true})
 var io = require("socket.io")(server)
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 
+
 app.use(express.json())
 
 app.use(function(req, res, next) {
@@ -54,26 +55,40 @@ app.listen(port, () => {
 })
 
 io.on("connection", socket => {
-  // socket.on('messageDialog', (data) => {
-  //   console.log(data)
-  //   io.emit('messageDialog', `${data.authorLogin}: ${data.message}` )
-  // })
-  io.of("/myDialogs").on("connection", socketDialog => {
-    // socketDialog.join('join',data.idDialog)
-    // socketDialog.disconnect(data.idDialog)
-    socketDialog.on("messageDialog", async data => {
-      socketDialog.join("join", data.idDialog)
-      socketDialog.disconnect(data.idDialog)
-      const message = await message_controller.addMessage(data)
-      console.log(message)
-      io.of("/myDialogs")
-        .to(data.idDialog)
-        .emit("messageDialog", message)
-    })
-
-    // socketDialog.disconnect(data.idDialog)
-  })
+  let idRoom  
+  socket.on("join", data => {
+    idRoom= data._id    
+    socket.join(idRoom)             
+  })  
+  socket.on("messageDialog", async data => {    
+    const  message = await message_controller.addMessage(data) 
+        
+    io.to(idRoom).emit("messageDialog2", message)        
+  })   
+  socket.on('end', ()=>{      
+    socket.leave(idRoom)   
+    
+  }) 
 })
+
 
 const serverPort = 8000
 io.listen(serverPort)
+
+// io.on("connection", socket => {
+//   // console.log("22")
+   
+//   socket.on("join", data => {
+    
+//     socket.join(data._id )
+//     socket.on("messageDialog", async data2 => {    
+//       const  message = await message_controller.addMessage(data2)    
+//       io.to(data._id ).emit("messageDialog2", message)   
+      
+//     }) 
+//     socket.on('end', ()=>{
+//       socket.leave(data._id)           
+//     })    
+//   })  
+  
+// })
