53ab606d5530aedc4144831ff116e83a644df91b
Ihar Tsykala <Sudja09@bk.ru>
2020-04-01 21:26:31 +0300

    heroku6

--- app.js
+++ app.js
@@ -1,22 +1,73 @@
 const express = require("express")
-const app = express()
+const routerUsers = require("./users/router-users")
+const routerPets = require("./pets/router-pets")
+const routerAlbums = require("./albums/router-albums")
+const routerUpload = require("./upload/router-upload")
+const routerPhotos = require("./photos/router-photos")
+const routerSubscriptions = require("./subscriptions/router-subscriptions")
+// const routerFriends = require("./friends/router-friends")
+const routerDialogs = require("./dialogs/router-dialogs")
+const routerMessages = require("./messages/router-messages")
+const MessageController = require("./messages/controller-messages")
+const message_controller = new MessageController()
+const mongoose = require("mongoose")
+require("dotenv").config()
+const cors = require("cors")
 
-const PORT = process.env.PORT || 80
+// mongoose.connect(process.env.BD, {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+//   useUnifiedTopology: true
+// })
 
-app.get("/", (req, res) => {
-  res.end(`<div>
-                <h1>Home page</h1>
-                <a href="/about">About</a>
-            </div>`)
+var app = require("express")()
+// var server = require("http").Server(app)
+// var io = require("socket.io")(server)
+const port = process.env.PORT || 8080
+
+app.use(express.json())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  )
+  next()
 })
 
-app.get("/about", (req, res) => {
-  res.end(`<div>
-                <h1>About page</h1>
-                <a href="/">Home</a>
-            </div>`)
+app.use(cors())
+
+// app.use("/users", routerUsers)
+// app.use("/pets", routerPets)
+// app.use("/albums", routerAlbums)
+// app.use("/photos", routerPhotos)
+// app.use("/subscriptions", routerSubscriptions)
+// app.use("/friends", routerFriends)
+// app.use("/dialogs", routerDialogs)
+// app.use("/messages", routerMessages)
+// app.use(express.static(__dirname + "/public"), routerUpload)
+
+app.listen(port, () => {
+  console.log("server on port " + port)
 })
 
-app.listen(PORT, () => {
-  console.log(`listen port: ${PORT}`)
-})
+// io.on("connection", socket => {
+//   let idRoom
+//   socket.on("join", data => {
+//     idRoom = data._id
+//     socket.join(idRoom)
+//   })
+//   socket.on("messageDialog", async data => {
+//     const message = await message_controller.addMessage(data)
+//     io.to(idRoom).emit("receiveMessageDialog", message)
+//   })
+//   socket.on("end", () => {
+//     socket.leave(idRoom)
+//   })
+// })
+
+// const serverPort = 8000
+// io.listen(serverPort)
